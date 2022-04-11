e8e5ce236665e4619c59f6bd55e36c6bb648d1e7
Chris Coyier <chriscoyier@gmail.com>
2022-04-11 09:24:11 -0700

    excercise 3

---
+++ 3/variables.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
+	// Experiments
+	f += "pie" // Tried := at first to see if that was similar
+	fmt.Println(f)
+
+	f = f + " is good" // can do it either way I guess
+	fmt.Println(f)
+}
+
+// Notes
+
+// It's funny how autosave will totally remove a line like `import "fmt"` if you save the file before you've typed a line that uses it.
+
+// Questions
+
+// Is there any other variable initialization keywords besides `var`? Like `const` and such?
